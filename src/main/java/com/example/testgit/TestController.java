865e207342137c0d4a9d122d925aa08736ee9fb8
yaojiehua <yaojiehua@pooksh.com>
2022-05-12 14:43:40 +0800

    [add]

--- src/main/java/com/example/testgit/TestController.java
+++ src/main/java/com/example/testgit/TestController.java
@@ -2,4 +2,8 @@ package com.example.testgit;
 
 public class TestController {
     public static final String WORD_TEST = "word_test";
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
